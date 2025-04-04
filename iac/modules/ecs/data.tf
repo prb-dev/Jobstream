09ee76a33ba61a61062ac8b9ffb33da05fb9ec20
Prabashwara <prabashwara.in@gmail.com>
2025-04-04 17:34:57 +0530

    Add SSM access to ECS task execution role (#14)
    * Add SSM access to ECS task execution role

--- iac/modules/ecs/data.tf
+++ iac/modules/ecs/data.tf
@@ -8,3 +8,14 @@ data "aws_iam_policy_document" "assume_role_policy" {
     }
   }
 }
+
+data "aws_iam_policy_document" "ssm_access_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["ssm:GetParameters", "secretsmanager:GetSecretValue"]
+    resources = [
+      aws_ssm_parameter.database_uri.arn,
+      aws_ssm_parameter.jwt_secret.arn
+    ]
+  }
+}
