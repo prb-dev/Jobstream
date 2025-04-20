4dfdaaea481bb5e07c513ad028bdb32a080118e2
Prabashwara <prabashwara.in@gmail.com>
2025-04-20 11:27:32 +0530

    Feat/access control (#18)
    * Update verifyCandidate.middleware.js
    
    * Update verifyEmployer.middleware.js

--- api/src/middlewares/verifyEmployer.middleware.js
+++ api/src/middlewares/verifyEmployer.middleware.js
@@ -1,3 +1,6 @@
+import { customError } from "../utils/error.util.js";
+import { logger } from "../utils/logger.util.js";
+
 export const verifyEmployer = (req, res, next) => {
   if (req.user.role !== "employer") {
     const error = customError(403, "Action is forbidden");
